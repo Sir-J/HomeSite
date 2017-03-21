55056da78a38dbc7252b6ec19a328161f9dd8548
Sergey Rogachev <erwin@sibmail.ru>
2017-03-21 23:23:43 +0700

    Базовый роутинг
    Сделан базовый роутинг

---
+++ HomeSite/app/about/about.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: '/app/about/about.component.html'
+})
+export class AboutComponent {
+    
+}
--- HomeSite/app/app.module.ts
+++ HomeSite/app/app.module.ts
@@ -1,21 +1,41 @@
-import { NgModule }      from '@angular/core';
+import { NgModule, NgModuleFactoryLoader, SystemJsNgModuleLoader }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import {trace, Category, UIRouterModule, UIView} from "ui-router-ng2";
+
+import { APP_STATES } from "./app.states";
 
 import { AppComponent }  from './app.component';
 import { AppHeaderComponent } from './app.header.component'
 import { AppFooterComponent } from './app.footer.component'
+import { MainComponent } from './main/main.component'
+import { AboutComponent } from './about/about.component'
 
 @NgModule({
   imports:      [ 
                     BrowserModule, 
-                    FormsModule
+                    UIRouterModule.forRoot({
+                      states: APP_STATES,
+                      otherwise: { state: 'main' },
+                      useHash: true,
+                      //config: routerConfigFn,
+                }),
+                BrowserModule,
+                FormsModule,
+                HttpModule
                 ],
   declarations: [ 
                     AppComponent,
                     AppHeaderComponent,
-                    AppFooterComponent
+                    AppFooterComponent,
+                    MainComponent,
+                    AboutComponent
                 ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+    { provide: NgModuleFactoryLoader, useClass: SystemJsNgModuleLoader }
+  ],
+  bootstrap:    [ UIView ]
 })
 export class AppModule { }
