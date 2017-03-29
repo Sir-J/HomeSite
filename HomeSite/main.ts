9336036631cce589d0922189936f4d7179e221a1
RogachevSE <RogachevSE@ugsk.ru>
2017-03-29 13:15:51 +0700

    master * откат изменений

--- HomeSite/main.ts
+++ HomeSite/main.ts
@@ -2,5 +2,4 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
