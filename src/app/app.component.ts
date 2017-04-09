7abee41537b8394b371c3cd7233e2c51a2699f6a
Sergey Rogachev <erwin@sibmail.ru>
2017-04-09 13:40:52 +0700

    version-0.1.2 * Доработка главной страницы * ДОбавлены Bootstrap UI компоненты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public myInterval: number = 5000;
+  public slides: any[] = [];
+  public activeSlideIndex: number;
+  public noWrapSlides: boolean = false;
+
+  constructor() {
+    for (let i = 0; i < 4; i++) {
+      this.slides.push({
+        image: `assets/slides/slide${i+1}.jpg`
+      });
+    }
+  }
 }
